05cefabae8449ddd88c969c7bdf220fe0f0b2050
fktryyy <dimasbatian1717@gmail.com>
2025-05-26 09:09:57 +0700

    url

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\FileDownloadController;
 
 Route::get('/', [LoginController::class, 'index']);
 Route::post('/submit', [LoginController::class, 'loginform'])->name('login.submit');
+Route::get('/submit', function () {
+    return redirect('/')->with('error', 'Akses tidak valid. Silakan login melalui form.');
+});
 Route::get('/download/{encodedUrl}', [FileDownloadController::class, 'download'])
     ->where('encodedUrl', '.*') // supaya URL bisa panjang
     ->name('download.file');
